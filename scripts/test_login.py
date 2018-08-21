1a522315eeb0e31b9c3d3e8231581c8c844eeed8
delilu <40994071+delilu@users.noreply.github.com>
2018-08-21 20:45:21 +0800

    Update test_login.py

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -13,10 +13,9 @@ class TestLogin:
         assert 1
 
     def test_login5(self):
-<<<<<<< HEAD
+
         assert 0
 
     def test_login6(self):
         assert 0
-=======
->>>>>>> f8cdbb34f993ed4bd8340cfe0ecc5681c419b13a
+
